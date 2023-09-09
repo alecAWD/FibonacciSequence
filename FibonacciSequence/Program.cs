4fb846d71cf71c420a6eca102d20089dfdb3a6cf
Alec Dady <alec.dady9@gmail.com>
2023-09-09 15:54:16 -0400

    Add files via upload

---
+++ FibonacciSequence/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FibonacciSequence
+{
+    internal class Program {
+        int Fib(int n) {
+            if(n <= 1) {
+                return n;
+            }
+            Console.WriteLine("Fib: " + n + " : " + (n - 1) + " : " + (n - 2));
+            return Fib(n - 1) + Fib(n - 2);
+        }
+        public static void Main(string[] args) {
+            Console.WriteLine("Enter n for the sequence...");
+            string? n = Console.ReadLine();
+            int num = int.Parse(n);
+            Program program = new Program();
+            Console.WriteLine("Result: " + program.Fib(num));
+        }
+    }
+}
